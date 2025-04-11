1ec1eb921e591d1faebc48cc33d4e99a226dbb5d
gideonadeti <gideonadeti0@gmail.com>
2025-04-11 12:06:53 +0000

    refactor(products): move auth guards to class level
    Refactor the ProductsController to apply authentication guards at the class level for better code organization and readability.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -21,12 +21,12 @@ import { RolesGuard } from 'src/roles/roles.guard';
 import { FindAllProductsDto } from './dto/find-all-products.dto';
 
 @ApiTags('products')
-@ApiBearerAuth()
-@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
@@ -43,12 +43,16 @@ export class ProductsController {
     return this.productsService.findOne(id);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
