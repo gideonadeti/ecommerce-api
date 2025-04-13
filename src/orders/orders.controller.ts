3573446adf50157e91abf890563f5c1ee1c7d8a0
gideonadeti <gideonadeti0@gmail.com>
2025-04-13 16:24:12 +0000

    feat(orders): add authentication guards to orders controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { OrdersService } from './orders.service';
 import { UserId } from 'src/user-id/user-id.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @ApiTags('orders')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
